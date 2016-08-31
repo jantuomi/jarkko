47568a1b661faa858658215e9cd8382e2b95c5c3
Jan Tuomi <jans.tuomi@gmail.com>
2016-08-31 18:23:44 +0000

    Add file reading

--- main.cpp
+++ main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+#include <fstream>
+#include <streambuf>
+#include <string>
+
+std::string readFile(std::string& filename) {
+    std::ifstream t(filename);
+    std::string str((std::istreambuf_iterator<char>(t)),
+                    (std::istreambuf_iterator<char>()));
+    return str;
+}
 
 int main(int argc, char** argv) {
+    if (argc != 2) {
+        std::cout << "Please supply input file name as a parameter." << std::endl;
+        return 1;
+    }
+
+    std::string filename = std::string(argv[1]);
+    std::string input = readFile(filename);
+    std::cout << input;
+
     return 0;
 }
