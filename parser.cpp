c59f10a0b0f58840c02327fc602a681c5410b212
Jan Tuomi <jans.tuomi@gmail.com>
2016-08-31 18:50:16 +0000

    Add string support

--- parser.cpp
+++ parser.cpp
@@ -5,8 +5,27 @@
 Parser::Parser(std::string& input) {
     std::vector<std::string> result;
 
+    bool isString = false;
+    std::string tmp;
     for (auto& c : input) {
-        std::string parsed(1, c);
-        result.push_back(parsed);
+        if (c == '"' && !isString) {
+            isString = true;
+            continue;
+        }
+
+        if (c == '"' && isString) {
+            result.push_back(tmp);
+            tmp = "";
+            isString = false;
+            continue;
+        }
+
+        if (!isString) {
+            std::string parsed(1, c);
+            result.push_back(parsed);
+        }
+        else {
+            tmp += c;
+        }
     }
 }
